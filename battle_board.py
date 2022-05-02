5dad964f66fe0335ed9ddea47f06252d50428c54
Humm <jhumm@mcw.edu>
2022-05-01 19:49:32 -0500

    feat(python) Created Initial Classes

--- battle_board.py
+++ battle_board.py
@@ -1,4 +1,16 @@
 class Battle_Board():
     def __init__(self) -> None:
+        self.row_label = ['A','B','C','D','E','F','G','H','I','J','K'/
+            'L','M','N','O','P','Q','R','S','T']
+        self.column_label = [str(range(1,21))]
         
+        self.grid = []
+        for row in range(20):
+             for column in range(20):
+                 self.grid[row][column] = '.'
+
+
         pass
+
+
+self = Battle_Board()
---
+++ battleship.py
@@ -0,0 +1,3 @@
+class Battleship():
+    def __init__(self) -> None:
+        pass
